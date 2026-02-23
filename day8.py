3ff331c12a1cdbbc1a537d8b130fa8ec4cd318e3
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-23 22:21:22 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,70 @@
+# #IF ELSE conditions
+
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You are eligible to vote.")
+# else:
+#     print("You are not eligible to vote.")
+
+
+# # Discount checker
+# amount = 1500
+# if amount > 1000:
+#     print("You get a discount!")
+# else:
+#     print("No discount for you.")
+
+# # If elif else(Multiple conditions)
+# marks = int(input("Enter your marks: "))
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 80:
+#     print("Grade: B")
+# elif marks >= 70:
+#     print("Grade: C")
+# elif marks >= 60:
+#     print("Grade: D")
+# else:
+#     print("Fail")
+
+
+# # Sales performance (Data Analyst Use Case)
+# sales = float(input("Enter sales amount: "))
+# if sales >= 10000:
+#     print("Excellent performance!")
+# elif sales >= 5000:
+#     print("Good performance!")
+# elif sales >= 1000:
+#     print("Average performance!")
+# else:
+#     print("Poor performance!")
+
+
+
+# #string comparison Example
+# City = "Delhi"
+# if City == "Delhi":
+#     print("City matched")
+# else:
+#     print("City not matched")
+
+
+# password = input("Enter your password: ")
+# if password == "Admin@123":
+#     print("Login successful!")
+# else:
+#     print("Incorrect password. Please try again.")
+
+# # Email Validation
+# emil = "user@example.com"
+# if "@" in emil and "." in emil:
+#     print("Valid email address.")
+# else:
+#     print("Invalid email address.")
+
+# #Advanced: Missing Data Check
+# value = ""
+# if value == "33":
+#     print("Missing data found")
+# else:
+#     print("Data Available")
