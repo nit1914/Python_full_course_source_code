374ab87bdbaaa812397ceb31366b49a0bbdca4f0
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-24 22:49:01 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,42 @@
+# # 9-Python-Nested IF and MUltiple conditions
+
+# print("Check your Elligiblity")
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     id_no = int(input("Enter your ID number: "))
+#     if id_no == 12345:
+#         print("You can enter")
+#     else:
+#         print("Invalid ID number")
+# else:
+#     print("You are underage.")
+
+
+# # Multiple conditions with AND
+
+# age = int(input("Enter your age: "))
+# residence = input("Enter your residence (City): ")
+# if age >= 18 and residence == "Delhi":
+#     print("You are eligible to vote in Delhi.")
+# else:
+#     print("You are not eligible to vote in Delhi.")
+
+
+# age = int(input("Enter your age: "))
+# residence = input("Are you Indian? :")
+# if age >= 18 and residence.lower() == "yes":
+#     print("Eligible to drive.")
+# else:
+#     print("Not eligible to drive.")
+
+# # Multiple conditions with OR
+
+# age = int(input("Enter your age: "))
+# residence = input(" Do you have licence? :")
+# if age >= 18 or residence.lower() == "yes":
+#     print("Eligible to register.")
+# else:
+#     print("Not eligible to register.")
+
+
+
