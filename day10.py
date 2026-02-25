0c312dd7607ffd5a38a916f77e75834e3444f81e
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-25 13:14:02 +0530

    Add files via upload

--- day10.py
+++ day10.py
@@ -25,13 +25,10 @@ If access level is less than 5 → print "Insufficient access level."'''
 
 
 # security_code = int(input("Enter the security code: "))
-
 # if security_code == 5566:
 #     department = input("Enter your department: ")
-
 #     if department.lower() == "finance":
 #         access_level = int(input("Enter your access level: "))
-
 #         if access_level >= 5:
 #             print("Access Granted: Welcome to the meeting room.")
 #         else:
@@ -72,4 +69,5 @@ Login
 #     else:
 #         print("Subject not available.")
 # else:
+
 #     print("Registration failed.")
