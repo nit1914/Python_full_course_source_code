af9d30771ca4c486ca686ed8f3194a862c8b9aa3
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-02-26 23:16:16 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,70 @@
+# Day 11 - For Loop
+
+# # 1 Basic Loop
+
+# for i in range (1,11):
+#     print(i)
+
+# 2 Print Characters
+# word = "Python"
+
+# for alphabet in word:
+#     print(alphabet)
+
+# word2 = "SQL"
+# for j in range(1,11):
+#     print(word2)
+
+# 3 Loop through list
+# items = ["Pen", "Notebook", "Eraser"]
+# for item in items:
+#     print(item)
+
+# # 4 Even numbers
+# print("print even numbers from 0 to 20")
+# for n in range(0,21,2):
+#     print(n)
+
+# # 5 Odd numbers
+# print("print odd numbers from 1 to 20")
+# for n in range(1,21,2):
+#     print(n)
+
+# # 6 Total Calculation
+# marks = [78,82,90]
+# total = 0
+# for m in marks:
+#     total += m
+# print("Total:", total)
+
+# # 7 Clean city names
+# cities = ["MUMbai"," pune  ","  CHENNAI"]
+# cleaned = []
+# for c in cities:
+#     cleaned.append(c.strip().title())
+# print(cleaned)
+
+
+# # 8 Loop with If Condition
+# nums = [5, 12,3,18,7]
+# for n in nums:
+#     if n >10:
+#         print(n, "is greater than 10.")
+#     else:
+#         print(n, "is not greater than 10.")
+
+
+
+# # 9 Loop with If Condition
+# nums = [5, 12,3,18,7]
+# for n in nums:
+#     if n % 2 == 0:
+#         print(n, "is even.")
+#     else:
+#         print(n, "is odd.")
+
+
+# # 10 Extract last digit from IDs
+# ids = ["EMP-001122", "EMP-889900"]
+# for last4 in ids:
+#     print(last4[-4:])
