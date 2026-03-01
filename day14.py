4305dfa15080c6b98f6f09a36a7f070cb16e2917
Nitesh Verma <nitesh.300012824041@csvtu.ac.in>
2026-03-01 09:50:34 +0530

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,41 @@
+#Day-14 Tuples
+
+# Creating tuples
+# fruits = ("Apple", "Banana", "Grapes")
+# print(fruits) 
+
+# # Indexing
+# print(fruits[0]) 
+# print(fruits[1]) 
+
+# # Slicing
+# nums = (10, 20, 30, 40, 50)
+# print(nums[1:4])  
+
+
+# # Looping
+# colors = ("Red", "Green", "Blue")
+# for c in colors:
+#     print(c)
+
+# Tuples length
+# print(len(colors)) 
+
+# # Concatenation
+# a = (1, 2, 3)
+# b = (4, 5, 6)
+# c = a + b
+# print(c) 
+
+
+# # Packing and Unpacking
+# data = ("Alice", 30, "Engineer")
+# name, age, profession = data
+# print(name, age, profession) 
+# print(f"name: {name}, age: {age}, profession: {profession}")
+
+
+# # Nested tuples
+# employee = [("John", 28, "Developer"), ("Alice", 30, "Designer"), ("Bob", 25, "Tester")]
+# for name, age, profession in employee:
+#     print(name, age, profession)
